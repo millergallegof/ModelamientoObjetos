1cd4c029c64e7c7af3b7025e8f5e6b9f2d51c189
millergallegof <90480065+millergallegof@users.noreply.github.com>
2022-02-17 18:38:21 -0500

    Desarrollo de Ejercicio No 2

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,13 @@
+
+package ejercicio2;
+
+public class Ejercicio2 {
+
+     static void main(String[] args) {
+        
+         
+         
+         System.out.println("Esta clse no se va ejecutar hasta que se cambie directamente en las configiraciones de netbeans ");
+    }
+    
+}
---
+++ Ejercicio2/src/ejercicio2/MainSecundario.java
@@ -0,0 +1,16 @@
+package ejercicio2;
+
+
+
+
+public class MainSecundario {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Se implementa la clase principal, para ejecutar el main desde esta clase");
+        System.out.println("el programa se puede jecutar sin embargo el IDE vincula unicamente una de las clases como una clase principal");
+        System.out.println("Es importante señalar que se puede tener 2 clases main en el mismo proyecto pero se debe indicar al IDE cual es la clase principal");
+
+    }
+
+}
